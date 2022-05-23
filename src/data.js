29794d9fa9332cd127a88705d945ce39a6f7e83e
Teodor <teodor1497@gmail.com>
2022-05-23 23:13:46 +0300

    add Slider's data

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: "https://mini-tigre.com/wp-content/uploads/2021/09/Shopping-1.jpg",
+        title: "SUMMER SALE",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "f5fafd"
+    },
+    {
+        id: 2,
+        img: "https://mini-tigre.com/wp-content/uploads/2021/09/Shopping-1.jpg",
+        title: "WINTER SALE",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "fcf1ed"
+    },
+    {
+        id: 3,
+        img: "https://mini-tigre.com/wp-content/uploads/2021/09/Shopping-1.jpg",
+        title: "DRYN DRYN SALE",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "f5fafd"
+    }
+]
