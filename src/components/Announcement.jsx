14c0d517eec5fe5a31fafc83c90587fa585dd37c
Teodor <teodor1497@gmail.com>
2022-05-23 11:51:31 +0300

    add style for announcement

--- src/components/Announcement.jsx
+++ src/components/Announcement.jsx
@@ -5,6 +5,11 @@ const Container = styled.div`
     height:30px;
     background-color:teal;
     color:white;
+    display:flex;
+    justify-content:center;
+    align-items:center;
+    font-size:14px;
+    font-weight:500;
 `
 
 
