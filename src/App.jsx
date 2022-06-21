f0743611214ad045e4387828022bbb608239a4cd
Teodor <teodor1497@gmail.com>
2022-06-21 16:42:57 +0300

    add routing

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,36 @@ import Product from './pages/Product';
 import Register from './pages/Register';
 import Login from './pages/Login';
 import Cart from './pages/Cart';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate
+} from "react-router-dom";
 
 function App() {
+  const user = true;
   return (
-    <div className="container">
-      <Product/>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+      <Routes>
+        <Route path='/products/:category' element={<ProductList />} />
+      </Routes>
+      <Routes>
+        <Route path='/product/:id' element={<Product />} />
+      </Routes>
+      <Routes>
+        <Route path='/cart' element={<Cart />} />
+      </Routes>
+      <Routes>
+        <Route path='/login' element={ user ? <Navigate to='/'/> : <Login />} />
+      </Routes>
+      <Routes>
+        <Route path='/register' element={user ? <Navigate to='/'/> : <Register />} />
+      </Routes>
+    </Router>
   );
 }
 
