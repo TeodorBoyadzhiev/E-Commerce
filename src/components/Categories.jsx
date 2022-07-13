2b5154cda89bb189bef2ba4b15fd2a5fcb5d6530
Teodor <teodor1497@gmail.com>
2022-07-13 22:29:09 +0300

    add title

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -10,16 +10,27 @@ const Container = styled.div`
     padding: 20px;
     justify-content:space-between;
     ${mobile({ padding: "0px", flexDirection: "column" })};   
+`;
 
+const Title = styled.h2`
+  font-size: bold;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-decoration-line: underline;
+  margin-top: 50px;
 `;
 
 const Categories = () => {
   return (
-    <Container>
-      {categories.map((item) => (
-        <CategoryItem item={item} key={item.id} />
-      ))}
-    </Container>
+    <>
+      <Title>Shop by category</Title>
+      <Container>
+        {categories.map((item) => (
+          <CategoryItem item={item} key={item.id} />
+        ))}
+      </Container>
+    </>
   )
 }
 
