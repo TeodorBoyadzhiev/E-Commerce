204ea19537fc580024f37cab27542d24f4937d2f
Teodor <teodor1497@gmail.com>
2023-01-03 22:35:14 +0200

    add validation

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,13 @@
+import React from 'react';
+//api
 import axios from 'axios';
-import React, { useState } from 'react';
+//styling
 import styled from 'styled-components';
+//responsive
 import { mobile } from '../responsive';
+//react hooks
+import { useForm } from 'react-hook-form';
+//util
 import BackButton from '../components/partials/BackButton';
 
 const Container = styled.div`
@@ -20,7 +26,7 @@ const Container = styled.div`
 `;
 const Wrapper = styled.div`
   position: relative;
-  width:40%;
+  width:35%;
   padding:40px 20px 20px 30px;
   background-color:white;
   ${mobile({ width: "75%" })};   
@@ -33,27 +39,34 @@ const Title = styled.h1`
   font-weight:300;
 `;
 const Form = styled.form`
-  display:grid;
-  grid-template-columns: 1fr 1fr;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
 `;
 const InputField = styled.div`
   display: flex;
+  flex-wrap: wrap;
   position: relative;
+  flex-basis: 48%;
   margin: 0px 10px 20px 0px;
+  & > input {
+    border: ${props=>props.hasError ? '2px solid red' : ''};
+    outline: ${props=>props.hasError ? 'red' : ''};
+  }
 `;
 const Input = styled.input`
   flex:1;
   min-width:40%;
-  margin: 0px 10px 20px 0px;
   padding:10px;
+  height: fit-content;
   &:focus + Label,
   &:valid + Label {
-      transform: translate(5px, -7px);
-      color: black;
-      font-size: 0.75em;
-      background-color: white;
-      transition: 0.2s ease-in-out;
-    }
+    transform: translate(5px, -7px);
+    color: black;;
+    font-size: 0.75em;
+    background-color: white;
+    transition: 0.2s ease-in-out;
+  }
 `;
 const Label = styled.label`
   position: absolute;
@@ -74,18 +87,28 @@ const Button = styled.button`
   color:white;
   cursor:pointer;
 `;
+const Error = styled.span`
+  color: red;
+`;
 
 const Register = () => {
-  const [name, setName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [username, setUsername] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPass, setConfirmPass] = useState('');
-
-  const handleRegister = async (e) => {
-    e.preventDefault();
+  const { register, handleSubmit, formState: { errors } } = useForm({
+    mode: 'onChange',
+    reValidateMode: 'onChange',
+    defaultValues: {
+      firstName: '',
+      lastName: '',
+      username: '',
+      email: '',
+      password: '',
+      confirmPassword: ''
+    }
+  });
+  const handleRegister = async (formValues) => {
     try {
+      const username = formValues.username;
+      const email = formValues.email;
+      const password = formValues.password;
       const res = await axios.post('http://localhost:5000/api/auth/register', { username, email, password });
     } catch (err) {
       console.log(err)
@@ -96,32 +119,50 @@ const Register = () => {
       <Wrapper>
         <BackButton />
         <Title>CREATE AN ACCOUNT</Title>
-        <Form>
-          <InputField>
-            <Input id="name" required/>
-            <Label htmlFor="name">Name</Label>
+        <Form onSubmit={handleSubmit(handleRegister)} noValidate>
+          <InputField hasError={errors.firstName ? true : false}>
+            <Input
+              id="firstName" 
+              {...register('firstName', { required: 'This filed is required', minLength: {value: 11, message: 'Required at least 11 charcters'} })} 
+              required />
+              <Label htmlFor="firstName">First Name</Label>
+              {errors.firstName && <Error>{errors.firstName.message}</Error>}
           </InputField>
-          <InputField>
-            <Input id="Last name" required/>
-            <Label htmlFor="Last name">Last name</Label>
+          <InputField hasError={errors.lastName ? true : false}>
+            <Input
+              id="lastName"
+              {...register('lastName', { required: 'This filed is required', minLength: {value: 7, message: 'Required at least 7 charcters'} })}
+              required />
+            <Label htmlFor="lastName">Last name</Label>
+            {errors.lastName && <Error>{errors.lastName.message}</Error>}
           </InputField>
-          <InputField>
-            <Input id="Username" required/>
-            <Label htmlFor="Username">Username</Label>
+          <InputField hasError={errors.username ? true : false}>
+            <Input
+              id="username"
+              {...register('username', { required: 'This filed is required', minLength: {value: 4, message: 'Required at least 4 charcters'} })}
+              required />
+              <Label htmlFor="username">Username</Label>
+              {errors.username ? <Error>{errors.username.message}</Error> : ''}
           </InputField>
-          <InputField>
-            <Input id="Email" required/>
-            <Label htmlFor="Email">Email</Label>
+          <InputField hasError={errors.email ? true : false}>
+            <Input
+            id="email" {...register('email', { required: 'This filed is required', pattern: {value: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-z]+\.(com|bg|net)$/, message: 'This email is invalid'}})} required />
+            <Label htmlFor="email">Email</Label>
+            {errors.email ? <Error>{errors.email.message}</Error> : ''}
           </InputField>
-          <InputField>
-            <Input id="Password" required/>
-            <Label htmlFor="Password">Password</Label>
+          <InputField hasError={errors.password ? true : false}>
+            <Input
+            id="password" {...register('password', { required: 'This filed is required', minLength: {value: 5, message: 'Required at least 5 charcters'} })} required />
+            <Label htmlFor="password">Password</Label>
+            {errors.password ? <Error>{errors.password.message}</Error> : ''}
           </InputField>
-          <InputField>
-            <Input id="Confirm Password" required/>
-            <Label htmlFor="Confirm Password">Confirm Password</Label>
+          <InputField hasError={errors.confirmPassword ? true : false}>
+            <Input
+            id="confirmPassword" {...register('confirmPassword', { required: 'This filed is required', minLength: {value: 5, message: 'Required at least 5 charcters'} })} required />
+            <Label htmlFor="confirmPassword">Confirm Password</Label>
+            {errors.confirmPassword ? <Error>{errors.confirmPassword.message}</Error> : ''}
           </InputField>
-          <Button onClick={handleRegister}>CREATE</Button>
+          <Button type='submit'>CREATE</Button>
           <Agreement>
             By creating an account, I consent to the processing of my personal
             data in accordance with the <b>PRIVACY POLICY</b>
