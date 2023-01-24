6af3c433677d6075ced6f35a22a0630ce3ad32ec
Teodor <teodor1497@gmail.com>
2023-01-24 22:52:32 +0200

    refactoring

--- src/pages/Product.style.js
+++ src/pages/Product.style.js
@@ -3,40 +3,40 @@ import styled from 'styled-components';
 //responsive
 import { mobile } from '../responsive';
 
-export const Container = styled.div``;
-export const Wrapper = styled.div`
+const Container = styled.div``;
+const Wrapper = styled.div`
   padding:50px;
   display:flex;
   ${mobile({ padding: "10px", flexDirection: "column" })};   
 
 `;
-export const ImgContainer = styled.div`
+const ImgContainer = styled.div`
   flex:1;
 `;
-export const Image = styled.img`
+const Image = styled.img`
   width:100%;
   height:90vh;
   object-fit:cover;
   ${mobile({ height: "40%" })};   
 
 `;
-export const InfoContainer = styled.div`
+const InfoContainer = styled.div`
   flex:1;
   padding:0px 50px;
   ${mobile({ padding: "10px" })};   
 
 `;
-export const Title = styled.h1`
+const Title = styled.h1`
   font-weight:200;
 `;
-export const Desc = styled.p`
+const Desc = styled.p`
   margin:20px 0px;
 `;
-export const Price = styled.span`
+const Price = styled.span`
   font-weight:100;
   font-size:40px;
 `;
-export const FilterContainer = styled.div`
+const FilterContainer = styled.div`
   display:flex;
   justify-content:space-between;
   flex-wrap: wrap;
@@ -46,16 +46,16 @@ export const FilterContainer = styled.div`
   ${mobile({ width: "100%" })};   
 
 `;
-export const Filter = styled.div`
+const Filter = styled.div`
   display:flex;
   align-items:center;
   gap: 7px;
 `;
-export const FilterTitle = styled.span`
+const FilterTitle = styled.span`
   font-size:20px;
   font-weight:200;
 `;
-export const FilterColor = styled.div`
+const FilterColor = styled.div`
   width:20px;
   height:20px;
   background-color: ${props => props.color};
@@ -65,7 +65,7 @@ export const FilterColor = styled.div`
     padding: 2px;
   };
 `;
-export const FilterSizeOption = styled.div`
+const FilterSizeOption = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
@@ -82,7 +82,7 @@ export const FilterSizeOption = styled.div`
     background: papayawhip;
   }
 `;
-export const AddContainer = styled.div`
+const AddContainer = styled.div`
   width:50%;
   margin-top:100px;
   display:flex;
@@ -91,12 +91,12 @@ export const AddContainer = styled.div`
   ${mobile({ width: "100%" })};   
 
 `;
-export const AmountContainer = styled.div`
+const AmountContainer = styled.div`
   display:flex;
   align-items:center;
   font-weight:700;
 `;
-export const Amount = styled.span`
+const Amount = styled.span`
   width:30px;
   height:30px;
   border-radius:10px;
@@ -106,7 +106,7 @@ export const Amount = styled.span`
   justify-content:center;
   margin:0 5px;
 `;
-export const Button = styled.button`
+const Button = styled.button`
   padding:15px;
   border:1.5px solid teal;
   background-color:white;
@@ -117,3 +117,23 @@ export const Button = styled.button`
     background-color:#f8f4f4;
   }
 `;
+
+export {
+  Container,
+  Wrapper,
+  ImgContainer,
+  Image,
+  InfoContainer,
+  Title,
+  Desc,
+  Price,
+  FilterContainer,
+  Filter,
+  FilterTitle,
+  FilterColor,
+  FilterSizeOption,
+  AddContainer,
+  AmountContainer,
+  Amount,
+  Button
+}
