558ab3ae19559f944467f5a6bc39f22478c6dba0
Teodor <teodor1497@gmail.com>
2023-01-30 22:41:15 +0200

    add accordion info menu

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -9,6 +9,8 @@ import styled from 'styled-components';
 import { mobile } from '../responsive';
 // MUI
 import { Add, Remove } from '@mui/icons-material';
+import ErrorOutlineIcon from '@mui/icons-material/ErrorOutline';
+import SimpleAccordion from '../components/MUI/Accordion';
 //state
 import { useSelector } from 'react-redux';
 import { useDispatch } from 'react-redux';
@@ -52,15 +54,23 @@ const TopText = styled.span`
 `;
 const Bottom = styled.div`
     display:flex;
-    justify-content:space-between;
+    gap: 150px;
     padding: 20px;
     ${mobile({ flexDirection: "column" })};   
 `;
+const LeftSection = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 50px;
+`;
+const RightSection = styled.div`  
+`;
 const Info = styled.div`
-    flex-basis: 60%;
     height: fit-content;
     background-color: white;
     border-radius: 10px;
+`;
+const AccordionWrapper = styled.div`
 `;
 const Product = styled.div`
     display:flex;
@@ -122,17 +132,25 @@ const Hr = styled.hr`
     height:1px;
     width: 30%;
 `;
+const Warning = styled.div`
+    display: flex;
+    padding: 10px;
+`;
+const NotReserved = styled.div`
+    margin-left: 5px;
+`;
 const Summary = styled.div`
     position: sticky;
     top: 20px;
-    flex-basis: 30%;
     border-radius: 10px;
     padding:20px;
-    height:50vh;
+    height: fit-content;
     background-color: white;
+    flex-basis: 30%;
 `;
 const SummaryTitle = styled.h1`
     font-weight:200;
+    margin-top: 0;
 `;
 const SummaryItem = styled.div`
     margin:30px 0px;
@@ -187,64 +205,75 @@ const Cart = () => {
                     <TopButton type="filled">CHECKOUT NOW</TopButton>
                 </Heading>
                 <Bottom>
-                    <Info>
-                        {cart.products.map((product, index) => (<React.Fragment key={index}>
-                            <Product key={product.id}>
-                                <ProductDetail>
-                                    <Image src={product.img} />
-                                    <Details>
-                                        <ProductName><b>Product:</b> {product.title}</ProductName>
-                                        <ProductId><b>ID:</b> 932131255</ProductId>
-                                        <ProductColor color={product.color} />
-                                        <ProductSize><b>Size:</b> {product.size}</ProductSize>
-                                        <RemoveButton handleClick={() => deleteProduct(product)} />
-                                    </Details>
-                                </ProductDetail>
-                                <PriceDetail>
-                                    <ProductAmountContainer>
-                                        <Add onClick={() => adjustQuantity('increase', product._id)} style={{ cursor: "pointer" }} />
-                                        <ProductAmount>{product.quantity}</ProductAmount>
-                                        <Remove onClick={() => adjustQuantity('decrease', product._id)} style={{ cursor: "pointer" }} />
-                                    </ProductAmountContainer>
-                                    <ProductPrice>$ {product.quantity ? product.price * product.quantity : product.price}</ProductPrice>
-                                </PriceDetail>
-                            </Product>
-                            <Hr />
-                        </React.Fragment>
-                        ))
-                        }
-                    </Info>
-                    <Summary>
-                        <SummaryTitle>ORDER SUMMARY</SummaryTitle>
-                        <SummaryItem>
-                            <SummaryItemText>Subtotal</SummaryItemText>
-                            <SummaryItemPrice>$ {cart.total ? cart.total : 0}</SummaryItemPrice>
-                        </SummaryItem>
-                        <SummaryItem>
-                            <SummaryItemText>Estimated Shipping</SummaryItemText>
-                            <SummaryItemPrice>$ 5.90</SummaryItemPrice>
-                        </SummaryItem>
-                        <SummaryItem>
-                            <SummaryItemText>Shipping Discount</SummaryItemText>
-                            <SummaryItemPrice>$ -5.90</SummaryItemPrice>
-                        </SummaryItem>
-                        <SummaryItem type="total">
-                            <SummaryItemText>Total</SummaryItemText>
-                            <SummaryItemPrice>$ {cart.total ? cart.total : 0}</SummaryItemPrice>
-                        </SummaryItem>
-                        <StripeCheckout
-                            name="Tushiba Shop"
-                            image="https://avatars.githubusercontent.com/u/1486366?v=4"
-                            billingAddress
-                            shippingAddress
-                            description={`Your total is $${cart.total}`}
-                            amount={cart.total * 100}
-                            token={onToken}
-                            stripeKey={KEY}
-                        >
-                            <SummaryButton>CHECKOUNT NOW</SummaryButton>
-                        </StripeCheckout>
-                    </Summary>
+                    <LeftSection>
+                        <Info>
+                            {cart.products.map((product, index) => (<React.Fragment key={index}>
+                                <Product key={product.id}>
+                                    <ProductDetail>
+                                        <Image src={product.img} />
+                                        <Details>
+                                            <ProductName><b>Product:</b> {product.title}</ProductName>
+                                            <ProductId><b>ID:</b> 932131255</ProductId>
+                                            <ProductColor color={product.color} />
+                                            <ProductSize><b>Size:</b> {product.size}</ProductSize>
+                                            <RemoveButton handleClick={() => deleteProduct(product)} />
+                                        </Details>
+                                    </ProductDetail>
+                                    <PriceDetail>
+                                        <ProductAmountContainer>
+                                            <Add onClick={() => adjustQuantity('increase', product._id)} style={{ cursor: "pointer" }} />
+                                            <ProductAmount>{product.quantity}</ProductAmount>
+                                            <Remove onClick={() => adjustQuantity('decrease', product._id)} style={{ cursor: "pointer" }} />
+                                        </ProductAmountContainer>
+                                        <ProductPrice>$ {product.quantity ? product.price * product.quantity : product.price}</ProductPrice>
+                                    </PriceDetail>
+                                </Product>
+                                <Hr />
+                            </React.Fragment>
+                            ))
+                            }
+                            <Warning className='warning'>
+                                <ErrorOutlineIcon />
+                                <NotReserved>PRODUCTS ARE NOT RESERVED</NotReserved>
+                            </Warning>
+                        </Info>
+                        <AccordionWrapper>
+                            <SimpleAccordion />
+                        </AccordionWrapper>
+                    </LeftSection>
+                    <RightSection>
+                        <Summary>
+                            <SummaryTitle>ORDER SUMMARY</SummaryTitle>
+                            <SummaryItem>
+                                <SummaryItemText>Subtotal</SummaryItemText>
+                                <SummaryItemPrice>$ {cart.total ? cart.total : 0}</SummaryItemPrice>
+                            </SummaryItem>
+                            <SummaryItem>
+                                <SummaryItemText>Estimated Shipping</SummaryItemText>
+                                <SummaryItemPrice>$ 5.90</SummaryItemPrice>
+                            </SummaryItem>
+                            <SummaryItem>
+                                <SummaryItemText>Shipping Discount</SummaryItemText>
+                                <SummaryItemPrice>$ -5.90</SummaryItemPrice>
+                            </SummaryItem>
+                            <SummaryItem type="total">
+                                <SummaryItemText>Total</SummaryItemText>
+                                <SummaryItemPrice>$ {cart.total ? cart.total : 0}</SummaryItemPrice>
+                            </SummaryItem>
+                            <StripeCheckout
+                                name="Tushiba Shop"
+                                image="https://avatars.githubusercontent.com/u/1486366?v=4"
+                                billingAddress
+                                shippingAddress
+                                description={`Your total is $${cart.total}`}
+                                amount={cart.total * 100}
+                                token={onToken}
+                                stripeKey={KEY}
+                            >
+                                <SummaryButton>CHECKOUNT NOW</SummaryButton>
+                            </StripeCheckout>
+                        </Summary>
+                    </RightSection>
                 </Bottom>
             </Wrapper>
             <Footer />
