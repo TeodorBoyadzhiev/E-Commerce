43a710018c59dce34a606e3e47f8e1f08566db95
Teodor <teodor1497@gmail.com>
2023-02-01 23:49:25 +0200

    add announcement

--- src/components/Announcement.jsx
+++ src/components/Announcement.jsx
@@ -1,5 +1,7 @@
-import styled from '@emotion/styled'
-import React from 'react'
+import React, { Component } from 'react';
+import styled from '@emotion/styled';
+
+import Typical from 'react-typical';
 
 const Container = styled.div`
   height: 30px;
@@ -7,17 +9,32 @@ const Container = styled.div`
   color: white;
   display: flex;
   align-items: center;
-  justify-content: center;
+  justify-content: space-evenly;
   font-size: 14px;
   font-weight: 500;
+`;
+const Wrapper = styled.div`
 `;
 
 
 const Announcement = () => {
+
   return (
-    <Container>
-        Super Deal!Free Shipping on Orders Over $50
-    </Container>
+    <Wrapper>
+      <Container>
+        <Typical
+          steps={['Hello!', 3000, 'Super Deal!Free Shipping on Orders Over $50', 2000]}
+          loop={Infinity}
+          wrapper="p"
+        />
+        <Typical
+          steps={['Hello!', 3000, 'Super Deal!Free Shipping on Orders Over $50', 2000]}
+          loop={Infinity}
+          wrapper="p"
+        />
+      </Container>
+
+    </Wrapper>
   )
 }
 
