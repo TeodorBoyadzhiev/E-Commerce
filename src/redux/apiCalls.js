e7c3d7a7048a3220ce06e6f46acf4a5ba800eb85
Teodor <teodor1497@gmail.com>
2023-02-12 23:11:50 +0200

    add axios request interceptor and update user tokens

--- src/redux/apiCalls.js
+++ src/redux/apiCalls.js
@@ -1,7 +1,31 @@
 import axios from "axios";
-import { loginFailure, loginStart, loginSuccess, logout } from "./userRedux"
+import { loginFailure, loginStart, loginSuccess, logout, setNewUserData } from "./userRedux";
+import { useDispatch, useSelector } from "react-redux";
+import jwt_decode from 'jwt-decode';
 
+const axiosJWT = axios.create();
 const BASE_URL = 'http://localhost:5000/api/auth';
+
+axiosJWT.interceptors.request.use(async (config) => {
+    const user = useSelector(state => state.user.currUser);
+    const dispatch = useDispatch();
+
+    const decodedToken = jwt_decode(user.accessToken);
+    let currentDate = new Date();
+
+    if (decodedToken.exp * 1000 < currentDate.getTime()) {
+        const data = await refreshToken(dispatch, user);
+        config.headers["authorization"] = "Bearer " + data.accessToken;
+
+        dispatch(setNewUserData({ ...user, accessToken: data.accessToken, refreshToken: data.refreshToken }));
+    }
+
+    return config;
+},
+    (error) => {
+        return Promise.reject(error);
+    }
+);
 
 export const login = async (dispatch, user) => {
     dispatch(loginStart());
@@ -14,10 +38,21 @@ export const login = async (dispatch, user) => {
     }
 }
 
+export const refreshToken = async (dispatch, user) => {
+    try {
+        const res = await axios.post(`${BASE_URL}/refresh`, { token: user.refreshToken });
+        dispatch(setNewUserData({ ...user, accessToken: res.accessToken, refreshToken: res.refreshToken }));
+
+        return res.data;
+    } catch (err) {
+        console.log(err);
+    }
+}
+
 export const logoutUser = async (dispatch, user) => {
     try {
         await axios
-            .post(`${BASE_URL}/logout`, user)
+            .post(`${BASE_URL}/logout`, { token: user.refreshToken })
             .then(() => dispatch(logout()))
             .catch((error) => {
                 console.log(error);
@@ -25,7 +60,7 @@ export const logoutUser = async (dispatch, user) => {
                     console.log(error.response.data);
                 }
             });
-            
+
     } catch (err) {
         console.log(err)
     }
